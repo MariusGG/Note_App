da037274939ecc85fc9c468552224370b3d23944
mariusgg <marius.g.guei@gmail.com>
2019-01-18 00:55:23 +0000

    passing show all notes test

--- lib/notebook.rb
+++ lib/notebook.rb
@@ -6,7 +6,15 @@ class NoteBook
   end
 
   def add_new_note(title, message)
-    new_note = { title: title, message: message }
-    @logged_notes << new_note
+    @logged_notes << { title: title, message: message }
   end
+
+  def show_all_notes
+    array = []
+     @logged_notes.each do | k, v|
+       array << "#{ k[:title] } " "#{k[:message]}"
+     end
+     array.join(", ")
+  end
+
 end
